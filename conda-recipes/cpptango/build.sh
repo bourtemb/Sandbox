b6dbfdc1bd09ed451f3d9220b86717dcf8a9c7dc
Reynald Bourtembourg <reynald.bourtembourg@esrf.fr>
2020-03-19 13:49:20 +0100

    Add cpptango conda package recipe

---
+++ conda-recipes/cpptango/build.sh
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_VERBOSE_MAKEFILE=true \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DOMNI_BASE=$PREFIX \
+      -DZMQ_BASE=$PREFIX \
+      -DIDL_BASE=$PREFIX \
+      ..
+
+make -j 2 install
